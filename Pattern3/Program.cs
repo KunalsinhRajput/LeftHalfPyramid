93dcc800a6c82bd5cba41c8c4e2567b593437b93
Kunal-Rajput-18 <kunal4248rajput@gmail.com>
2024-11-05 12:33:56 +0530

    Add project files.

---
+++ Pattern3/Program.cs
@@ -0,0 +1,34 @@
+﻿//output is
+/*
+    ----*
+    ---**
+    --***
+    -****
+    *****
+ */
+
+
+
+namespace Pattern3
+{
+    class Program
+    { 
+        public static void Main(string[] args) 
+        {
+            for (int i =1; i <= 5; i++)
+            {
+                for (int j =i; j <=5; j++)
+                {
+                    Console.Write(" ");
+                }
+               
+                for (int k = 1; k <=i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
